05a9fd9b82c9f9f64d9d84751d0851989d208fe8
De Pecol Lorenzo <depecol.lorenzo@osdbsesto>
2020-11-26 12:55:26 +0100

    ottimizzazione script

--- es2.js
+++ es2.js
@@ -1,15 +1,12 @@
 var addBtn = function(){
-    var btn = $("<button id=\"btn1\">DO NOT CLICK</button>");
-    btn.on("click", changeColor);
-    $("#body1").append(btn);
-}
-
-var changeColor = function(){
-    var caccola = $("<h3>caccola</h3>");
-        $("#span1").html("CUCU");
-        $("#div1").html("<h1>TI AVEVO DETTO DI NON CLICCARE MALEDETTO</h1>");
-        $("#div1").attr("style", "background-color:   rgb(163, 36, 68)");
-        $("#body1").append(btn);
+    $("#body1").append( 
+    $("<button id=\"btn1\">DO NOT CLICK</button>")).on("click", function(){
+        var caccola = $("<h3>caccola</h3>");
+            $("#span1").html("CUCU");
+            $("#div1").html("<h1>TI AVEVO DETTO DI NON CLICCARE MALEDETTO</h1>");
+            $("#div1").attr("style", "background-color:   rgb(163, 36, 68)");
+            $("#body1").append(btn);
+    });
 }
 
 document.addEventListener("DOMContentLoaded", addBtn);
